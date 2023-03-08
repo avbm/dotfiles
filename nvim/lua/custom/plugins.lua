7620995d69e9f85f7ad24969c7f1d278f8572de7
Amod Mulay <amodm@splunk.com>
2023-03-07 22:01:18 -0800

    switch to nvim kickstart with which-key

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,9 @@
+
+-- puppet plugins
+use 'rodjek/vim-puppet'
+
+-- go related plugins
+use 'ray-x/go.nvim'
+use 'ray-x/guihua.lua' -- recommanded if need floating window support
+use 'neovim/nvim-lspconfig'
+use 'nvim-treesitter/nvim-treesitter'
